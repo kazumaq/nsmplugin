ebcee1fe6664766b0b3942685818a11a49a89fc4
Leon Verdin <leonverdin@Leons-MacBook-Pro.local>
2023-05-02 21:47:57 -0700

    Add first test case for grammar syntax

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -13,3 +13,68 @@ suite("Extension Test Suite", () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   });
 });
+
+import * as path from "path";
+import { Registry, INITIAL, IRawGrammar, IOnigLib } from "vscode-textmate";
+import { loadWASM, OnigScanner, OnigString } from "onigasm";
+
+// Load onigasm WASM binary and create onigLib
+async function loadOnigLib(): Promise<IOnigLib> {
+  await loadWASM(require("onigasm/lib/onigasm.wasm"));
+  return {
+    createOnigScanner: (sources) => new OnigScanner(sources),
+    createOnigString: (str) => new OnigString(str),
+  };
+}
+
+suite("NSM Grammar", () => {
+  test("Tokenization", async () => {
+    const onigLibPromise = loadOnigLib();
+    const registry = new Registry({
+      onigLib: onigLibPromise,
+      loadGrammar: async (scopeName: string) => {
+        if (scopeName === "source.nsm") {
+          const grammarPath = path.join(
+            __dirname,
+            "../../../..",
+            "syntaxes",
+            "nsm.tmLanguage.json",
+          );
+          const grammarContent = require(grammarPath);
+          return grammarContent as IRawGrammar;
+        }
+        return null;
+      },
+    });
+
+    // Load the grammar
+    const grammarPath = path.join(
+      __dirname,
+      "../../../..",
+      "syntaxes",
+      "nsm.tmLanguage.json",
+    );
+    const grammarContent = require(grammarPath);
+    const grammar = await registry.addGrammar(grammarContent as IRawGrammar);
+
+    // Sample code for testing
+    const sampleCode = "I WANT THIS THING";
+
+    // Initialize prevState as null
+    const prevState = INITIAL;
+
+    // Expected tokenization
+    const expectedTokens = [
+      { startIndex: 0, type: "entity.name.type" }, // I
+      { startIndex: 1, type: "support.function" }, // WANT
+      { startIndex: 6, type: "variable.parameter" }, // THIS
+      { startIndex: 11, type: "entity.name.type" }, // THING
+    ];
+
+    // Tokenize the sample code
+    const lineTokens = grammar.tokenizeLine(sampleCode, prevState).tokens;
+
+    // Compare the actual and expected tokenization
+    assert.deepStrictEqual(lineTokens, expectedTokens);
+  });
+});
