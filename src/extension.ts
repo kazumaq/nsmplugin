4f87219d628bc095975da90d0e4ee2666ab2552c
Leon Verdin <leonverdin@Leons-MacBook-Pro.local>
2023-05-04 10:37:07 -0700

    More changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "nsmplugin.helloWorld",
+    'nsmplugin.helloWorld',
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from nsmplugin!");
-    },
+      vscode.window.showInformationMessage('Hello World from nsmplugin!');
+    }
   );
 
   context.subscriptions.push(disposable);
