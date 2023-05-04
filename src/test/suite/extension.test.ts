3557cd42a733b63d564fa1efc8cb731709175107
Leon Verdin <leonverdin@Leons-MacBook-Pro.local>
2023-05-04 00:08:31 -0700

    Refactor and add comments

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,25 +1,25 @@
+// Import required modules
 import * as assert from "assert";
 import * as fs from "fs";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
-
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
-
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
-});
-
 import * as path from "path";
-import { Registry, INITIAL, IRawGrammar, IOnigLib } from "vscode-textmate";
+import * as vscode from "vscode";
+import {
+  IGrammar,
+  Registry,
+  INITIAL,
+  IRawGrammar,
+  IOnigLib,
+} from "vscode-textmate";
 import { loadWASM, OnigScanner, OnigString } from "onigasm";
 
+// onigasm is a WebAssembly port of the Oniguruma regular expression library.
+// It is used for tokenizing text based on TextMate grammars.
+// More about Oniguruma: https://github.com/kkos/oniguruma
+// More about onigasm: https://github.com/NeekSandhu/onigasm
+
 // Load onigasm WASM binary and create onigLib
+// onigLib is an object implementing IOnigLib interface, which is required by vscode-textmate
+// More about vscode-textmate: https://github.com/microsoft/vscode-textmate
 async function loadOnigLib(): Promise<IOnigLib> {
   const wasmPath = require.resolve("onigasm/lib/onigasm.wasm");
   const wasmBinary = fs.readFileSync(wasmPath);
@@ -32,20 +32,41 @@ async function loadOnigLib(): Promise<IOnigLib> {
   };
 }
 
+const SCOPE_NSM = "source.nsm";
+const GRAMMAR_PATH = path.join(
+  __dirname,
+  "../../..",
+  "syntaxes",
+  "nsm.tmLanguage.json",
+);
+
+// Define a test suite for the extension
+// Test suites help organize test cases for different aspects of the extension
+// More about Mocha test suites: https://mochajs.org/#suite
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
+
+  // Define a simple test case as an example
+  test("Sample test", () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
+
+// Define a test suite specifically for NSM Grammar
 suite("NSM Grammar", () => {
-  test("Tokenization", async () => {
+  let registry: Registry;
+  let grammar: IGrammar;
+
+  // Setup code to be executed before running any test in the suite
+  // This code initializes the Registry and loads the grammar for testing
+  suiteSetup(async () => {
     const onigLibPromise = loadOnigLib();
-    const registry = new Registry({
+    registry = new Registry({
       onigLib: onigLibPromise,
       loadGrammar: async (scopeName: string) => {
-        if (scopeName === "source.nsm") {
-          const grammarPath = path.join(
-            __dirname,
-            "../../..",
-            "syntaxes",
-            "nsm.tmLanguage.json",
-          );
-          const grammarContent = require(grammarPath);
+        if (scopeName === SCOPE_NSM) {
+          const grammarContent = require(GRAMMAR_PATH);
           return grammarContent as IRawGrammar;
         }
         return null;
@@ -53,64 +74,74 @@ suite("NSM Grammar", () => {
     });
 
     // Load the grammar
-    const grammarPath = path.join(
-      __dirname,
-      "../../..",
-      "syntaxes",
-      "nsm.tmLanguage.json",
-    );
-    const grammarContent = require(grammarPath);
-    const grammar = await registry.addGrammar(grammarContent as IRawGrammar);
+    const grammarContent = require(GRAMMAR_PATH);
+    grammar = await registry.addGrammar(grammarContent as IRawGrammar);
+  });
 
-    // Sample code for testing
-    const sampleCode = "I WANT THIS THING";
-
+  // Function to test tokenization of a given code snippet
+  // This function tokenizes the sample code using the grammar and compares the result with expected tokens
+  function testTokenization(sampleCode: string, expectedTokens: any) {
     // Initialize prevState as null
     const prevState = INITIAL;
-
-    // Expected tokenization
-    const expectedTokens = [
-      {
-        startIndex: 0,
-        endIndex: 1,
-        scopes: ["source.nsm", "entity.name.type"],
-      },
-      {
-        startIndex: 1,
-        endIndex: 2,
-        scopes: ["source.nsm"],
-      },
-      {
-        startIndex: 2,
-        endIndex: 6,
-        scopes: ["source.nsm", "support.function"],
-      },
-      {
-        startIndex: 6,
-        endIndex: 7,
-        scopes: ["source.nsm"],
-      },
-      {
-        startIndex: 7,
-        endIndex: 11,
-        scopes: ["source.nsm", "variable.parameter"],
-      },
-      {
-        startIndex: 11,
-        endIndex: 12,
-        scopes: ["source.nsm"],
-      },
-      {
-        startIndex: 12,
-        endIndex: 17,
-        scopes: ["source.nsm", "entity.name.type"],
-      },
-    ];
 
     // Tokenize the sample code
     const lineTokens = grammar.tokenizeLine(sampleCode, prevState).tokens;
 
     // Compare the actual and expected tokenization
     assert.deepStrictEqual(lineTokens, expectedTokens);
+  }
+
+  // Test case for tokenization
+  test("Tokenization", () => {
+    const sampleCode = "I WANT THIS THING";
+    const expectedTokens = [
+      {
+        startIndex: 0,
+        endIndex: 1,
+        scopes: [SCOPE_NSM, "entity.name.type"],
+      },
+      {
+        startIndex: 1,
+        endIndex: 2,
+        scopes: [SCOPE_NSM],
+      },
+      {
+        startIndex: 2,
+        endIndex: 6,
+        scopes: [SCOPE_NSM, "support.function"],
+      },
+      {
+        startIndex: 6,
+        endIndex: 7,
+        scopes: [SCOPE_NSM],
+      },
+      {
+        startIndex: 7,
+        endIndex: 11,
+        scopes: [SCOPE_NSM, "variable.parameter"],
+      },
+      {
+        startIndex: 11,
+        endIndex: 12,
+        scopes: [SCOPE_NSM],
+      },
+      {
+        startIndex: 12,
+        endIndex: 17,
+        scopes: [SCOPE_NSM, "entity.name.type"],
+      },
+    ];
+
+    testTokenization(sampleCode, expectedTokens);
   });
+
+  // Add more test cases here
+  // Example:
+  // test("Another Tokenization", () => {
+  //   const sampleCode = "SAMPLE CODE HERE";
+  //   const expectedTokens = [
+  //     // Expected tokenization structure
+  //   ];
+  //   testTokenization(sampleCode, expectedTokens);
+  // });
 });
