3a595b31d6d2e3a1d93c5b0e162aaf3c6cdb6747
Kazumaq <kazuma314159@outlook.com>
2023-05-25 17:05:05 -0700

    Adding changes to extension packaging for testing

--- scripts/package-and-copy.sh
+++ scripts/package-and-copy.sh
@@ -1,48 +1,65 @@
 #!/bin/bash
 
-# This script assumes the following:
-# 1. It's being run from the 'scripts' subdirectory in your project.
-# 2. Your project source code resides at '~/src/nsmplugin/'.
-# 3. You have npm, vsce, and webpack installed and available in your path.
-# 4. You're running this script on MacOS.
-# 5. Your VS Code is installed in the default location.
+# This script is used to package a VS Code extension, remove an older version if it exists,
+# and copy the new version to the VS Code extensions directory.
 
-# Define the path to your extension's source code and VS Code extensions directory
-EXTENSION_DIR=~/src/nsmplugin
-VSCODE_EXTENSIONS_DIR="~/Library/Application Support/Code/User/extensions"
+# Requirements:
+# - npm, vsce, and webpack command-line tools should be installed and available in the PATH.
+# - The VS Code extension project should be located in the '~/src/nsmplugin' directory.
+# - The VS Code extensions directory should be '~/.vscode/extensions'.
 
-# Change to your extension directory
-echo "Changing to directory: $EXTENSION_DIR"
-cd $EXTENSION_DIR
+# Notes:
+# - If an older version of the extension .vsix file exists in the VS Code extensions directory, it will be removed.
 
-# Check if npm, vsce, and webpack are installed and available in your path
-command -v npm >/dev/null 2>&1 || { echo >&2 "npm is required but it's not installed.  Aborting."; exit 1; }
-command -v vsce >/dev/null 2>&1 || { echo >&2 "vsce is required but it's not installed.  Aborting."; exit 1; }
-command -v webpack >/dev/null 2>&1 || { echo >&2 "webpack is required but it's not installed.  Aborting."; exit 1; }
+extension_dir=~/src/nsmplugin
+vscode_extensions_dir=~/.vscode/extensions
+abort_msg="Operation failed. Aborting."
+
+echo "Changing to directory: $extension_dir"
+cd "$extension_dir" || { echo "Unable to change to directory: $extension_dir. $abort_msg"; exit 1; }
+
+# Check for necessary command-line tools
+for cmd in npm vsce webpack
+do
+  command -v $cmd >/dev/null 2>&1 || { echo >&2 "$cmd is required but it's not installed. $abort_msg"; exit 2; }
+done
 echo "Confirmed npm, vsce, and webpack are installed."
 
-# If a dist directory exists (from previous builds), clean it up
-if [ -d "dist" ]; then
+if [[ -d "dist" ]]; then
     echo "Removing existing dist directory."
-    rm -r dist
+    rm -r dist || { echo "Failed to remove dist directory. $abort_msg"; exit 3; }
 fi
 
-# Compile TypeScript to JavaScript and bundle your code using webpack
 echo "Compiling TypeScript to JavaScript and running webpack to bundle JavaScript."
-npm run compile
+npm run compile || { echo "Failed to compile TypeScript to JavaScript. $abort_msg"; exit 4; }
 
-# Package your extension into a .vsix file using vsce
 echo "Packaging extension into .vsix file."
-vsce package
+vsce package || { echo "Failed to package extension. $abort_msg"; exit 5; }
 
-# Determine the most recently generated .vsix file
 vsix_file=$(ls -t *.vsix | head -1)
+target_vsix_file="$vscode_extensions_dir/$vsix_file"
 
-echo "Copying $vsix_file to $VSCODE_EXTENSIONS_DIR."
-cp $vsix_file "$VSCODE_EXTENSIONS_DIR"
+# Check if the .vsix file already exists in the target directory
+if [[ -f "$target_vsix_file" ]]; then
+    echo "$target_vsix_file already exists."
+    # Print file information
+    echo "File size: $(du -sh $target_vsix_file)"
+    echo "Last modification date: $(stat -f "%Sm" $target_vsix_file)"
 
-echo "Successfully copied $vsix_file to $VSCODE_EXTENSIONS_DIR."
+    echo "Deleting existing .vsix file."
+    rm "$target_vsix_file" || { echo "Failed to delete existing .vsix file. $abort_msg"; exit 6; }
+fi
+
+echo "Copying $vsix_file to $vscode_extensions_dir."
+
+cp "$vsix_file" "$vscode_extensions_dir" || { echo "Failed to copy $vsix_file to $vscode_extensions_dir. $abort_msg"; exit 7; }
+
+# Print new file information
+echo "New file size: $(du -sh $target_vsix_file)"
+echo "New file creation date: $(stat -f "%Sm" $target_vsix_file)"
+
+echo "Successfully copied $vsix_file to $vscode_extensions_dir."
 
 echo "To install the new extension, open VS Code, go to the Extensions view, \
 click on the More Actions (...) menu, select 'Install from VSIX...', \
-navigate to $VSCODE_EXTENSIONS_DIR, and select the .vsix file."
+navigate to $vscode_extensions_dir, and select the .vsix file."
